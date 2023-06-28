353da020009c215f6755c54b0c3239787ead20df
Karan Kumar <karankumark19k@gmail.com>
2023-06-28 13:36:52 +0530

    Successfully play playlist

--- app/src/main/java/com/example/streamify/MainActivity.kt
+++ app/src/main/java/com/example/streamify/MainActivity.kt
@@ -1,18 +1,36 @@
 package com.example.streamify
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.streamify.ui.theme.StreamifyTheme
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
+import com.spotify.android.appremote.api.error.CouldNotFindSpotifyApp
+import com.spotify.android.appremote.api.error.NotLoggedInException
+import com.spotify.android.appremote.api.error.UserNotAuthorizedException
+import com.spotify.protocol.types.Track
+import com.spotify.sdk.android.auth.AccountsQueryParameters.CLIENT_ID
+import com.spotify.sdk.android.auth.AuthorizationClient
+import com.spotify.sdk.android.auth.AuthorizationRequest
+import com.spotify.sdk.android.auth.AuthorizationResponse
+import com.spotify.sdk.android.auth.LoginActivity.REQUEST_CODE
+
 
 class MainActivity : ComponentActivity() {
+
+    private val clientId = "836af8984b5b45f6ad52e19fae6e2dfc"
+    private val redirectUri = "streamify://callback"
+    private var spotifyAppRemote: SpotifyAppRemote? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +40,99 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+
+
                 }
             }
         }
+
+
     }
+    private val connectionListener = object : Connector.ConnectionListener {
+        override fun onConnected(appRemote: SpotifyAppRemote) {
+            spotifyAppRemote = appRemote
+            Log.d("MainActivity", "Connected! Yay!")
+            // Now you can start interacting with App Remote
+            connected()
+        }
+
+        override fun onFailure(throwable: Throwable) {
+            Log.e("MainActivity", throwable.message, throwable)
+            // Something went wrong when attempting to connect! Handle errors here
+            if (throwable is NotLoggedInException || throwable is UserNotAuthorizedException){
+                triggerLoginFlow()
+            }else if ( throwable is CouldNotFindSpotifyApp ){
+                Toast.makeText(this@MainActivity,"Download Spotify app first", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        SpotifyAppRemote.disconnect(spotifyAppRemote)
+
+        val connectionParams = ConnectionParams.Builder(clientId)
+            .setRedirectUri(redirectUri)
+            .showAuthView(true)
+            .build()
+
+
+        SpotifyAppRemote.connect(this, connectionParams, connectionListener )
+    }
+
+    private fun connected() {
+        spotifyAppRemote?.let {
+            // Play a playlist
+            val playlistURI = "spotify:playlist:37i9dQZF1DX2sUQwD7tbmL"
+            it.playerApi.play(playlistURI)
+            // Subscribe to PlayerState
+            it.playerApi.subscribeToPlayerState().setEventCallback {
+                val track: Track = it.track
+                Log.d("MainActivity", track.name + " by " + track.artist.name)
+            }
+        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        spotifyAppRemote?.let {
+            SpotifyAppRemote.disconnect(it)
+        }
+
+    }
+
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            val response = AuthorizationClient.getResponse(resultCode, intent)
+            when (response.type) {
+                AuthorizationResponse.Type.TOKEN -> {}
+                AuthorizationResponse.Type.ERROR -> {}
+                else -> {}
+            }
+        }
+    }
+    fun triggerLoginFlow(){
+
+        val REQUEST_CODE = 1337
+        val REDIRECT_URI = redirectUri
+
+        val builder =
+            AuthorizationRequest.Builder(CLIENT_ID, AuthorizationResponse.Type.TOKEN, REDIRECT_URI)
+
+        builder.setScopes(arrayOf("streaming"))
+        val request = builder.build()
+
+        AuthorizationClient.openLoginActivity(this, REQUEST_CODE, request)
+
+    }
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StreamifyTheme {
-        Greeting("Android")
-    }
-}
